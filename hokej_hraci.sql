f79b4df895b3b5a0038ecbe05b9696fe737a95c5
JakubPlha <153103635+JakubPlha@users.noreply.github.com>
2025-10-22 13:12:47 +0200

    Add files via upload

---
+++ hokej_hraci.sql
@@ -0,0 +1,34 @@
+CREATE TABLE hockeyPlayer (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fname VARCHAR(30) NOT NULL,
+    lname VARCHAR(50) NOT NULL,
+    team VARCHAR(50) NOT NULL
+);
+
+DELIMITER //
+
+CREATE PROCEDURE insert_player(
+    IN p_fname VARCHAR(30),
+    IN p_lname VARCHAR(50),
+    IN p_team VARCHAR(50)
+)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        SELECT "Chyba" AS zprava;
+    END;
+
+    START TRANSACTION;
+
+    INSERT INTO hockeyPlayer (fname, lname, team)
+    VALUES (p_fname, p_lname, p_team);
+
+    COMMIT;
+END //
+
+DELIMITER ;
+
+CALL insert_player("David", "Pastrňák", "Boston Bruins");
+
+CALL insert_player("Pavel", "Zacha", NULL);
